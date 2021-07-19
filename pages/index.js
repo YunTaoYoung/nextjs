e0f89f5eff0c0fcbf35bf97bef59f744b0dc58cc
Yuntao <20303001+YunTaoYoung@users.noreply.github.com>
2021-07-19 11:44:54 +0800

    Hello next

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          World is so big.
         </p>
 
         <div className={styles.grid}>
